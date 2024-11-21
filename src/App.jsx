5341d5e66eb81f372daae27ad297da2fff3934c5
davidwrossiter <davewrossiter@gmail.com>
2024-11-21 09:29:22 +0000

    init git

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="main-section">
+        <div className="app-body">
+          <h1>My to do list app</h1>
+          <input type="text" id="taskInput" placeholder="Enter a new task" />
+          <button id="addTaskButton" onClick={() => setCount(count + 1)}>
+            Add Task
+          </button>
+          <ul id="taskList">{count}</ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
