9528aceee5eae8b72a3460b5ad18b3239a1e0447
davidwrossiter <davewrossiter@gmail.com>
2024-11-21 09:42:59 +0000

    added conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [lastClicked, setLastClicked] = useState(true);
 
   return (
     <>
@@ -10,13 +11,28 @@ function App() {
         <div className="app-body">
           <h1>My to do list app</h1>
           <input type="text" id="taskInput" placeholder="Enter a new task" />
-          <button id="increase count" onClick={() => setCount(count + 1)}>
+          <button
+            id="increase count"
+            onClick={() => {
+              setCount(count + 1);
+              setLastClicked(true);
+            }}
+          >
             Add Task
           </button>
-          <button id="decrease count" onClick={() => setCount(count - 1)}>
+          <button
+            id="decrease count"
+            onClick={() => {
+              setCount(count - 1);
+              setLastClicked(false);
+            }}
+          >
             Add Task
           </button>
           <ul id="taskList">{count}</ul>
+          <p>
+            {lastClicked ? "Clicked increased last" : "Clicked decrease last"}
+          </p>
         </div>
       </div>
     </>
